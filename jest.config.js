c811fd7453e71f656123e5718b32daa67bd07f38
Md Najim Ahmed <najim.ju@gmail.com>
2022-08-18 14:39:35 +0800

    feat: added code coverage

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,14 @@ module.exports = {
   testMatch: [
     "/**/*.test.ts"
   ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
+  "mapCoverage": true,
+  "verbose": false
 };
